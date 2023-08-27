637bc1abd2d5916b25eec664d7c9553ec90e8268
tudastuk <129007673+tudastuk@users.noreply.github.com>
2023-08-28 02:45:56 +0300

    3

---
+++ async-requests/progressbar/task.js
@@ -0,0 +1,14 @@
+const progress = document.getElementById( 'progress' );
+const form = document.getElementById('form');
+
+form.addEventListener('submit', (e) => {
+    const formData = new FormData(form);
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/upload');
+    xhr.upload.onprogress = function(event) {
+        progress.value = event.loaded / event.total;
+    }
+
+    xhr.send(formData);
+    e.preventDefault();
+});
